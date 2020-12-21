032ec0cb64861b041523b4d76679d4f45cd2d36b
Ben Ridley <benridley3@gmail.com>
2020-12-21 15:35:25 +0000

    routes and controllers for courses and bookings done

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,41 @@
+class BookingsController < ApplicationController
+  before_action :find_booking, only: [:show]
+  before_action :find_course, only: [:create]
+  before_action :find_user, only: [:create, :index]
+
+  def index
+    @my_bookings = current_user.bookings.where.order(deadline: :desc)
+  end
+
+  def show
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = @user
+    @booking.course = @course
+    if @booking.save!
+      redirect_to bookings_path
+    else
+      render '/courses/show'
+    end
+  end
+
+  private
+
+  def find_user
+    @user = current_user
+  end
+
+  def find_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def find_course
+    @course = Service.find(params[:service_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:deadline)
+  end
+end
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,3 +1,50 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
+  def index
+    @courses = Course.all
+  end
+
+  def show
+    @booking = Booking.new
+  end
+
+
+  #use pundit to only allow admin to do these methods
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      redirect_to course_path(@course)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @course.update(course_params)
+    redirect_to course_path(@course)
+  end
+
+  def destroy
+    @course.destroy
+    redirect_to root_path
+  end
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :type, :paid, :description, :example)
+  end
 
 end
