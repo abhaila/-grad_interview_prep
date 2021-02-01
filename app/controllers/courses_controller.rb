28667bbf42c5aec3e71302087c8e0fd9b64fcd7a
Anthony Hadwan <anthony.hadwan@hec.edu>
2021-02-01 20:28:15 +0200

    added file upload

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -48,6 +48,6 @@ class CoursesController < ApplicationController
   end
 
   def course_params
-    params.require(:course).permit(:name, :course_type, :paid_material, :description, :example_question, :photo)
+    params.require(:course).permit(:name, :course_type, :paid_material, :description, :example_question, :photo, :files)
   end
 end
