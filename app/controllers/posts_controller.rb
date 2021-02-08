7d7e557977146ddd14f63bf53d5815f650b30dfc
Megan Wong <meganwong117@gmail.com>
2021-02-08 20:50:16 +0800

    posts controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,16 +4,28 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
     authorise @post
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
+  def update
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, :summary, :photo)
   end
 end
