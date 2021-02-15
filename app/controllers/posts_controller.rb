29f334516b48bcd0dfd74abf3469d9778ecd513d
Tamara Cheikha <tamarasihamcheikha@gmail.com>
2021-02-15 20:05:34 +0000

    added some code to the posts controller hehe guys

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,16 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
     @posts = policy_scope(Post).order(created_at: :desc)
   end
 
+  def new
+    @post = Post.new
+    authorise @post
+  end
+
   def create
+    @post = Post.new(post_params)
     authorise @post
   end
 
