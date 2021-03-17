0d9f0b1bd9b501c5b836d10ff24a82050f8ed38e
Areeb Bhaila <areeb.bhaila@gmail.com>
2021-03-17 19:53:39 +0000

    completed the pundit for flashcards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,12 +2,14 @@ class CardsController < ApplicationController
   def new
     @deck = Deck.find(params[:deck_id])
     @card = Card.new
+    authorize @deck
   end
 
   def create
     @card = Card.new(card_params)
     @deck = Deck.find(params[:deck_id])
     @card.deck = @deck
+    authorize @deck
     if @card.save
       redirect_to deck_path(@deck)
     else
@@ -18,6 +20,6 @@ class CardsController < ApplicationController
   private
 
   def card_params
-    params.require(:cards).permit(:front_side, :back_side)
+    params.require(:card).permit(:front_side, :back_side)
   end
 end
